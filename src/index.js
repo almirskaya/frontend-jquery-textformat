325d42d81191162dd55f94091a56c109bb6a65b0
almirskaya <alexamirskaya@gmail.com>
2016-08-04 11:55:52 +0300

    index.js has been changed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 'use strict';
 
 console.log($)
+
+$(document).ready (function(){
+	$(".message-form .message-field").keyup (function(e) {
+		var textValue=$.trim(e.currentTarget.value).replace( /[\n\s]+/g, " ").toLowerCase();
+	$(".message-formatted").text(textValue);
+	});	
+});
